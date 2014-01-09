82e1c8089b4ff25f4fcf23febab81cc1b0ddd7f9
QuanYuan <quanyuan@seas.upenn.edu>
2014-01-09 13:38:23 -0800

    adding js

--- app/api.rb
+++ app/api.rb
@@ -4,4 +4,14 @@ class LinkedInApp < Sinatra::Base
   get '/hi' do
     'Minimal LinkedIn sinatra hello world!'
   end
+
+  get '/api/application.js' do
+    content_type 'text/javascript'
+    erb :'javascripts/linkedin_network.js'
+  end
+
+  get '/api/application.css' do
+    content_type 'text/css'
+    erb :'stylesheets/linkedin.css'
+  end
 end
